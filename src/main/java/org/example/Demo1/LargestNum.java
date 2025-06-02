e27941479760a9f716f28dd3880040c0630598cc
Krishna Deshmukh <krishnadesh2001@gmail.com>
2025-06-02 20:13:30 +0530

    interview questions

---
+++ src/main/java/org/example/Demo1/LargestNum.java
@@ -0,0 +1,27 @@
+package org.example.Demo1;
+
+import java.util.Arrays;
+
+public class LargestNum {
+    public static void main(String[] args) {
+
+
+
+        int []num={12,3,4,5,6,7,8};
+        int largest=num[0];
+        int smallest=num[0];
+
+        for(int i=1;i<num.length;i++){
+            if(num[i]>largest){
+                largest=num[i];
+            }
+            else if(num[i]<smallest){
+                smallest=num[i];
+            }
+        }
+        System.out.println(Arrays.toString(num));
+        System.out.println(largest);
+        System.out.println(smallest);
+
+    }
+}
